9e9934b5c0537997672e98106ff53e0fc8c06fa8
Mariela <smart.happy.face_2906@hotmail.com>
2025-09-02 00:45:35 +0300

    Controllers and services for prdct-initial added.

---
+++ backend/inventorysimulator/src/main/java/com/retailops/inventorysimulator/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.retailops.inventorysimulator.controller;
+
+import com.retailops.inventorysimulator.model.Product;
+import com.retailops.inventorysimulator.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+  private final ProductService productService;
+
+   @GetMapping
+    public List<Product> getAllProducts() {
+       return productService.findAll();
+   }
+
+}
