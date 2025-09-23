aa10fc3c2389cdd8ee2a3d37b06d92089879e6e5
Mariela <smart.happy.face_2906@hotmail.com>
2025-09-23 21:56:02 +0300

    Create an account table for user registration.

--- backend/inventorysimulator/src/main/java/com/retailops/inventorysimulator/config/SecurityConfig.java
+++ backend/inventorysimulator/src/main/java/com/retailops/inventorysimulator/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package com.retailops.inventorysimulator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +20,19 @@ public class SecurityConfig  {
                         .anyRequest().permitAll()  // allow all requests without login
                 );
         return http.build();
+    }
+
+//    @Bean
+//    public AuthenticationManager authManager(HttpSecurity http, PasswordEncoder encoder) throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(encoder)
+//                .and()
+//                .build();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
