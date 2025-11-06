d179ae5f70f98acc44d7811d3782024a2360fe26
Mariela <smart.happy.face_2906@hotmail.com>
2025-11-06 21:27:32 +0200

    Getting started with menu: content-navigation.

---
+++ frontend/inventorysim-app/src/components/layout/components/menu/Nav-Menu.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/Button"
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+import { MailIcon, PlusCircleIcon, type LucideIcon } from 'lucide-react'
+
+const NavMenuRoot = ({items}:
+{
+  items: {
+  title: string,
+  id?: string,
+  url: string,
+  icon?: LucideIcon
+}[]
+}
+) => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center gap-2">
+            
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton tooltip={item.title}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
+
+export default NavMenuRoot
