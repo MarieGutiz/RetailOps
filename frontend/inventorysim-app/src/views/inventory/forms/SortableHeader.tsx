348e88751b7b5e546a82c042b43c4e5ccf4c1d28
Mariela <smart.happy.face_2906@hotmail.com>
2025-11-19 20:53:16 +0200

    Tooltip for sorting header and table styling.

---
+++ frontend/inventorysim-app/src/views/inventory/forms/SortableHeader.tsx
@@ -0,0 +1,37 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+
+
+const SortableHeader = ({
+  column,
+  label,
+}: {
+  column: any;
+  label: string;
+}) => {
+  const isSorted = column.getIsSorted();
+  const arrow = isSorted ? (isSorted === "asc" ? "↑" : "↓") : "";
+
+   return (
+    <div className="flex justify-center">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              className={`toolbar-element jbtn-flat-btn toolbar-element-md active 
+                ${isSorted ? "sorted" : ""}`}
+              onClick={() => column.toggleSorting(isSorted === "asc")}
+            >
+              {label} {arrow}
+            </button>
+          </TooltipTrigger>
+
+          <TooltipContent side="bottom" className="text-xs">
+            Sort {isSorted === "asc" ? "descending" : "ascending"}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}
+
+export default SortableHeader
