f91abaa66ccbe929f914ffeed87cfe96699f9a09
Mariela <smart.happy.face_2906@hotmail.com>
2025-12-14 15:54:47 +0200

    Hero Section and footer added to home page.

---
+++ frontend/inventorysim-app/src/features/animation/HoverCard.tsx
@@ -0,0 +1,20 @@
+import { motion } from  "framer-motion"
+import  { Card, CardContent } from "@/components/ui/card"
+
+const HoverCard = ({title, description} : {title: string, description: string}) => {
+  return (
+    <motion.div
+        whileHover={{ y: -6, scale: 1.02 }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        >
+        <Card className="rounded-2xl shadow-sm hover:shadow-md transition-shadow">
+        <CardContent className="p-6 space-y-2">
+        <h3 className="font-semibold text-lg">{title}</h3>
+        <p className="text-sm text-muted-foreground">{description}</p>
+        </CardContent>
+        </Card>
+    </motion.div>
+  )
+}
+
+export default HoverCard
---
+++ frontend/inventorysim-app/src/features/homepage/HomePage.tsx
@@ -0,0 +1,16 @@
+
+import FooterHome from "./FooterHome"
+import HeroSection from "./HeroSection"
+
+const HomePage = () => {
+    return (
+       <>
+       <HeroSection />
+       <FooterHome />
+       </> 
+    )  }
+
+
+export default HomePage
+
+
